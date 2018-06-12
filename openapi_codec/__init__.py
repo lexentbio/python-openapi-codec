b8826f79aaa3f5dda3c64cd9cedd7e7527b368bf
Subhash Chandra <TMaYaD@LoonyB.in>
2018-06-12 19:48:58 +0530

    Version bump

--- openapi_codec/__init__.py
+++ openapi_codec/__init__.py
@@ -8,7 +8,7 @@ from openapi_codec.encode import generate_swagger_object
 from openapi_codec.decode import _parse_document
 
 
-__version__ = '1.3.2'
+__version__ = '1.3.3-patch.1'
 
 
 class OpenAPICodec(BaseCodec):
